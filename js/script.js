01c89d7a5787f4b0313966b8dec55e570f4a0027
tomaspa-dev <blord.dev@gmail.com>
2024-04-01 19:50:44 -0500

    agregando popup sliders al boton case study

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// JavaScript para abrir y cerrar el popup
 document.addEventListener('DOMContentLoaded', function() {
     let openPopupBtn = document.querySelector('.open-popup-btn');
     let popup = document.getElementById('popup');
@@ -6,14 +5,13 @@ document.addEventListener('DOMContentLoaded', function() {
   
     openPopupBtn.addEventListener('click', function() {
       popup.style.display = 'flex';
-      document.body.style.overflow = 'hidden'; // Evita el scroll en el body cuando está abierto el popup
+      document.body.style.overflow = 'hidden';
     });
   
     closePopupBtn.addEventListener('click', function() {
       closePopup();
     });
   
-    // Cerrar el popup al hacer clic fuera del contenido
     popup.addEventListener('click', function(event) {
       if (event.target === popup) {
         closePopup();
@@ -28,55 +26,53 @@ document.addEventListener('DOMContentLoaded', function() {
   
     function closePopup() {
       popup.style.display = 'none';
-      document.body.style.overflow = ''; // Habilita el scroll en el body cuando se cierra el popup
+      document.body.style.overflow = '';
     }
-  });
-
-  let list = document.querySelector('.slider .list');
-  let items = document.querySelectorAll('.slider .list .item');
-  let dots = document.querySelectorAll('.slider .dots li');
-  let prev = document. getElementById('prev');
-  let next = document. getElementById('next');
   
-  let active = 0;
-  let lengthItems = items.length - 1;
-
-  next.onclick = function() {
-    if (active + 1 > lengthItems) {
+    let list = document.querySelector('.slider-popup .list');
+    let items = document.querySelectorAll('.slider-popup .list .item');
+    let dots = document.querySelectorAll('.dots .dot');
+    let prev = document.getElementById('prev');
+    let next = document.getElementById('next');
+  
+    let active = 0;
+    let lengthItems = items.length - 1;
+  
+    next.onclick = function() {
+      if (active + 1 > lengthItems) {
         active = 0;
-    } else {
+      } else {
         active = active + 1;
+      }
+      reloadSlider();
     }
-    reloadSlider();
-  }
-
-  prev.onclick = function() {
-    if (active - 1 < 0) {
+  
+    prev.onclick = function() {
+      if (active - 1 < 0) {
         active = lengthItems;
-    } else {
+      } else {
         active = active - 1;
+      }
+      reloadSlider();
     }
-    reloadSlider();
-  }
-
-
-  let refreshSlider = setInterval(()=> {next.click()}, 5000);
-
-  function reloadSlider() {
-    let checkLeft = items[active].offsetLeft;
-    list.style.left = -checkLeft + 'px';
-
-    let lastActiveDot = document.querySelector(' .slider .dots li.active');
-    lastActiveDot.classList.remove('active');
-    dots[active].classList.add('active');
-    clearInterval(refreshSlider)
-    refreshSlider = setInterval(()=> {next.click()}, 5000);
-  }
-
-  dots.forEach((li, key) => {
-    li.addEventListener('click', function(){
-        active = key;
+  
+    let refreshSlider = setInterval(()=> {next.click()}, 3000);
+  
+    function reloadSlider() {
+      let checkLeft = items[active].offsetLeft;
+      list.style.transform = `translateX(-${checkLeft}px)`;
+  
+      dots.forEach(dot => dot.classList.remove('active'));
+      dots[active].classList.add('active');
+      clearInterval(refreshSlider);
+      refreshSlider = setInterval(()=> {next.click()}, 3000);
+    }
+  
+    dots.forEach((dot, index) => {
+      dot.addEventListener('click', function(){
+        active = index;
         reloadSlider();
-    })
-})
+      })
+    });
+  });
   
