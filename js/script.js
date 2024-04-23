1e06f509c5db7c47ac864a1897076c90a51f595f
tomaspa-dev <blord.dev@gmail.com>
2024-04-23 17:11:00 -0500

    agregando scripts y animaciones para el cambio de slides

--- js/script.js
+++ js/script.js
@@ -1,78 +1,76 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let openPopupBtn = document.querySelector('.open-popup-btn');
-    let popup = document.getElementById('popup');
-    let closePopupBtn = document.querySelector('.close-btn');
-  
-    openPopupBtn.addEventListener('click', function() {
-      popup.style.display = 'flex';
-      document.body.style.overflow = 'hidden';
+gsap.registerPlugin(CustomEase);
+gsap.registerPlugin(ScrollTrigger);
+
+var slide = 1;
+var pauseSlider = false;
+var progress = 0;
+
+/* Slide animations */
+var tl1 = gsap.timeline({paused: false});
+var ease = CustomEase.create("custom", "M0,0 C0.246,0.041 0.22,0.315 0.359,0.606 0.427,0.748 0.571,0.989 1,1 ")
+
+tl1.from(".featured-slide .featured-img-gif", {y:"110%", opacity: 0, ease: ease, duration: 1, scaleY: .5}, .7);
+
+tl1.from(".featured-slide .featured-img-box", {y:"110%", opacity: 0, ease: ease, duration: 1, scaleY: .5}, .9);
+
+tl1.fromTo(".featured-slide .featured-title .featured-secondary .featured-description", {y: '40%', opacity: 0, duration:1 , ease: 'power3.out'}, {y: '0%', opacity: 1},.9);
+
+
+function changeSlide(id) {
+    let slides = document.querySelectorAll(".featured-slide"); // Corregir el selector CSS
+    slides.forEach(slide => {
+        slide.classList.remove("active");
     });
-  
-    closePopupBtn.addEventListener('click', function() {
-      closePopup();
+
+    let newSlide = document.querySelector(".featured-slide.featured-slide" + id); // Corregir el selector CSS
+    newSlide.classList.add("active");
+
+    slide = id;
+
+    // Resetear controles a inactivos
+    let controls = document.querySelectorAll(".controls ul li");
+    controls.forEach((f) => {
+        f.classList.remove("active");
     });
-  
-    popup.addEventListener('click', function(event) {
-      if (event.target === popup) {
-        closePopup();
-      }
+
+    // Establecer nuevo activo
+    controls[id - 1].classList.add("active");
+
+    // Animate the transition
+    gsap.to(".featured-slide", {
+        opacity: 0,
+        duration: 0.5,
+        ease: "power2.inOut"
     });
-  
-    document.addEventListener('keydown', function(event) {
-      if (event.key === 'Escape') {
-        closePopup();
-      }
+    gsap.to(".featured-slide" + id, {
+        opacity: 1,
+        duration: 0.5,
+        ease: "power2.inOut"
     });
-  
-    function closePopup() {
-      popup.style.display = 'none';
-      document.body.style.overflow = '';
-    }
-  
-    let list = document.querySelector('.slider-popup .list');
-    let items = document.querySelectorAll('.slider-popup .list .item');
-    let dots = document.querySelectorAll('.dots .dot');
-    let prev = document.getElementById('prev');
-    let next = document.getElementById('next');
-  
-    let active = 0;
-    let lengthItems = items.length - 1;
-  
-    next.onclick = function() {
-      if (active + 1 > lengthItems) {
-        active = 0;
-      } else {
-        active = active + 1;
-      }
-      reloadSlider();
-    }
-  
-    prev.onclick = function() {
-      if (active - 1 < 0) {
-        active = lengthItems;
-      } else {
-        active = active - 1;
-      }
-      reloadSlider();
-    }
-  
-    let refreshSlider = setInterval(()=> {next.click()}, 1800);
-  
-    function reloadSlider() {
-      let checkLeft = items[active].offsetLeft;
-      list.style.transform = `translateX(-${checkLeft}px)`;
-  
-      dots.forEach(dot => dot.classList.remove('active'));
-      dots[active].classList.add('active');
-      clearInterval(refreshSlider);
-      refreshSlider = setInterval(()=> {next.click()}, 1800);
-    }
-  
-    dots.forEach((dot, index) => {
-      dot.addEventListener('click', function(){
-        active = index;
-        reloadSlider();
-      })
+}
+
+// Agregar eventos de clic a los controles derechos
+var controls = document.querySelectorAll(".controls ul li");
+for (let i = 0; i < controls.length; i++){
+    controls[i].addEventListener("click", () => {
+        changeSlide(i+1);
     });
-  });
-  
+}
+
+function startProgressBar() {
+    setInterval(() => {
+        if(pauseSlider) return;
+        progress += .1;
+
+        if(progress >= 8) {
+            changeSlide((slide % 4) + 1);
+            progress = 0;        
+        }
+        
+
+        gsap.to(".slideProgress", 
+        {scaleX: progress / 8, duration: .3});
+    },100);
+}
+
+// startProgressBar();
